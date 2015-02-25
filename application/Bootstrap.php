cec769ddb8a1bca93725a46b4ebe12c2d1b98fe7
Bakharevich <ilya@bakharevich.by>
2015-02-25 20:02:17 +0300

    Another fix with lowercase FS registry

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -25,7 +25,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     protected function _initLayoutHelper()
     {
-        $layoutLoader = new Phonebook_Controller_Action_Helper_LayoutLoader();
+        $layoutLoader = new Phonebook_Controller_Action_Helper_Layoutloader();
         Zend_Controller_Action_HelperBroker::addHelper($layoutLoader);
     }
 
